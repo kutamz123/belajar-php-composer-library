d3b446e912c53d1a4ed85f21d1c9b0bcbf1d9519
kutamz123 <andikautama034@gmail.com>
2022-06-14 13:15:10 +0700

    first

---
+++ src/Customer.php
@@ -0,0 +1,17 @@
+<?php
+
+class Customer
+{
+
+    private $nama;
+
+    public function __construct(string $nama)
+    {
+        $this->nama = $nama;
+    }
+
+    public function sayHello(string $nama)
+    {
+        return "Hello $nama, My name is $this->nama";
+    }
+}
